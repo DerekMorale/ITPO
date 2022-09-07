d99b5165246a5b1806082ef0c2ac95bdaf7d3cf3
DerekMorale <103907216+DerekMorale@users.noreply.github.com>
2022-09-07 10:55:49 -0600

    Add files via upload

---
+++ formularios.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Aceptar Datos</title>
+    </head>
+    <body>
+       <?php
+       echo "Los datos ingresados del usuario";
+       echo "         ";
+       echo "<br>";
+       echo $_REQUEST ['n1'];
+       echo "<br>";
+       echo $_REQUEST ['n2'];
+       echo "<br>";
+       echo $_REQUEST ['n3'];
+       ?>
+       </body>
+       </html>
+       
+
